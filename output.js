395e6de7056bbe929befe759d1562484c515f087
bjcoombs <bcoombs@gmail.com>
2022-10-08 21:06:39 +0100

    Committing latest changes

--- output.js
+++ output.js
@@ -1,7 +1,7 @@
 // mcpe_viz javascript helper file -- created by bedrock_viz program
 var worldName = 'Eloise_02';
 var worldSeed = -494460667990824802;
-var creationTime = 'Sat Oct  8 20:06:35 2022';
+var creationTime = 'Sat Oct  8 21:06:36 2022';
 var creationBedrockVizVersion = '0.1.7';
 var loadGeoJSONFlag = false;
 var fnGeoJSON = 'output.geojson';
